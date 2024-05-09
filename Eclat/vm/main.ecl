2455de059ca12972a72288190a819dd1b27c871e
Said Zuhair <m.masoud247@gmail.com>
2024-05-09 23:04:38 +0200

    test multiple_env

--- Eclat/vm/main.ecl
+++ Eclat/vm/main.ecl
@@ -100,7 +100,7 @@ let display_end cy =
 
 let main debug =
   (* chargement du programme *)
-  let is_loaded = load bc_delta in
+  let is_loaded = load bc_multiple_env in
 
   let cy = counter (is_loaded) in
 
--- MicroJS/tests/multiple_env.js
+++ MicroJS/tests/multiple_env.js
@@ -1,11 +1,14 @@
 var g = 10;
 
-function test2 (n2) {
-  return g + n2;
+function test2 (n4) {
+  return g + n4;
 }
 
-function test1 (n1) {
-  return test2(n1);
+function test1 (n1, n2, n3) {
+  let x = 10;
+  let y = 20;
+  let z = 30;
+  return test2(n1 + n2 + n3 + x + y + z);
 }
 
-test1(20);
+test1(20, 20, 20);
